2a7d539a39a3e137b9729c5b879932ff23a8ae37
eylommaayan <eylonmaayan@gmail.com>
2025-10-30 22:47:22 +0200

    server start

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,21 @@
-const express = require('express');
+
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes.js';
+
 
 const app = express();
 
-app.listen(5000, () => console.log('Server is running on port 5000'));
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+}); 
+
+
+app.use("/api/auth", authRoutes)
+
+
+
+app.listen(PORT, () => console.log(`Server is running on port 5000 ${PORT}`));
