7f7df74951abaed71059acb05abb507227868de8
eylommaayan <eylonmaayan@gmail.com>
2025-11-05 22:28:58 +0200

    user.model.js

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,27 @@
 // backend/server.js
-
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connecToMongoDB.js"; // ודא שהשם בקובץ עם ה-t
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// כדי ש-body יעבוד ב-POST /login
 app.use(express.json());
 
-// ראוט בדיקה ראשי
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// פה אנחנו מחברים את כל הראוטים של auth תחת /api/auth
 app.use("/api/auth", authRoutes);
 
-// מדליקים את השרת
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const start = async () => {
+  await connectToMongoDB();          // ← קודם DB
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+start();
